e6816350a3c0aeb87bda02730b28f6f609b5d891
Winters57 <89528658+Winters57@users.noreply.github.com>
2023-04-28 14:50:36 +0000

    IN class assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class Main {
-{   
+   
    public static void main(String[] args) 
    {  
       final int QUARTS_IN_GALLON = 4;
@@ -8,12 +8,27 @@ class Main {
       int gallonsNeeded;
       int extraQuartsNeeded;
       Scanner input = new Scanner(System.in);
-      System.out.print("Enter quarts needed >> ");
-      quartsNeeded = input.nextInt();
-      gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
-      extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
-      System.out.println("A job that needs " + quartsNeeded +
-         " quarts requires " + gallonsNeeded + " gallons plus " +
-         extraQuartsNeeded + " quarts.");
+      
+      for(int x=0; x<1;)
+        {
+         try
+          {
+            System.out.println("Enter quarts needed >> ");
+            quartsNeeded = input.nextInt();
+            gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
+            extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
+            System.out.println("A job that needs " + quartsNeeded +
+              " quarts requires " + gallonsNeeded + " gallons plus " +
+              extraQuartsNeeded + " quarts.");
+            
+          }
+          catch(Exception mistake)
+            {
+              System.out.println("Please snter a valid number >> ");
+            }
+          x = 2;
+          //loop is broken ran out of time'
+        }
+      
    }
 }
